676c60e1aa905b106d49be4b8d8a6a00d77012f7
foureverhh <foureverhh@gmail.com>
2018-11-04 23:14:07 +0100

    Chinese is recognized from photo

--- app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.zfgg04.getinfofromwikepediawithorcandtranslation;
 
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.services.vision.v1.Vision;
 import com.google.api.services.vision.v1.VisionRequestInitializer;
+import com.google.api.services.vision.v1.model.AnnotateImageRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesRequest;
+import com.google.api.services.vision.v1.model.BatchAnnotateImagesResponse;
+import com.google.api.services.vision.v1.model.Feature;
+import com.google.api.services.vision.v1.model.Image;
+import com.google.api.services.vision.v1.model.TextAnnotation;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageView;
+    TextView textView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.raw.bank_china);
+        imageView.setImageBitmap(bitmap);
 
         //Config Google Cloud Vision API
         Vision.Builder visionBuilder = new Vision.Builder(
@@ -21,7 +49,65 @@ public class MainActivity extends AppCompatActivity {
                 new AndroidJsonFactory(),
                 null);
         visionBuilder.setVisionRequestInitializer(new VisionRequestInitializer("AIzaSyDVm55Q1b9VB5ZqG-Hfd2WlbTtUmcmkVh8"));
-        visionBuilder.build();
+        final Vision vision = visionBuilder.build();
+
+        //Encode picture to Base64
+        //Create new thread
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Convert photo to byte array
+                InputStream inputStream = getResources().openRawResource(R.raw.bank_china);
+                try {
+                    byte[] photoData = IOUtils.toByteArray(inputStream);
+                    inputStream.close();
+                    Image inputImage = new Image();
+                    inputImage.encodeContent(photoData);
+
+                    //Making a request to the API and get response
+                    Feature textFeature = new Feature();
+                    textFeature.setType("DOCUMENT_TEXT_DETECTION");
+
+                    AnnotateImageRequest request = new AnnotateImageRequest();
+                    request.setImage(inputImage);
+                    request.setFeatures(Arrays.asList(textFeature));
+
+                    BatchAnnotateImagesRequest batchRequest = new BatchAnnotateImagesRequest();
+                    batchRequest.setRequests(Arrays.asList(request));
+                    BatchAnnotateImagesResponse batchResponse =
+                            vision.images().annotate(batchRequest).execute();
+
+                    //Using response
+                    final TextAnnotation text = batchResponse.getResponses().get(0).getFullTextAnnotation();
+
+                    //Show the text by Toast
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),text.getText(),Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            stringBuilder.append(text.getText());
+                            textView.setText(stringBuilder.toString());
+                        }
+                    });
+
+                    //textView.setText(text.getText());
+                    // android.view.ViewRootImpl$CalledFromWrongThreadException: Only the original thread that created a view hierarchy can touch its views.
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+
 
     }
 
