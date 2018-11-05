8a1f3c0b1160c2bc0bb9533ce78cdc312d489271
foureverhh <foureverhh@gmail.com>
2018-11-05 23:35:38 +0100

    Got labels from Google Vision API

--- app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/TranslateTextFromCamera.java
+++ app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/TranslateTextFromCamera.java
@@ -3,19 +3,30 @@ package com.example.zfgg04.getinfofromwikepediawithorcandtranslation;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.provider.MediaStore;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.github.kittinunf.fuel.Fuel;
+import com.github.kittinunf.fuel.core.FuelError;
+import com.github.kittinunf.fuel.core.Handler;
+import com.github.kittinunf.fuel.core.Request;
+import com.github.kittinunf.fuel.core.Response;
+
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+
+import kotlin.Pair;
 
 public class TranslateTextFromCamera extends AppCompatActivity {
 
@@ -42,70 +53,100 @@ public class TranslateTextFromCamera extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, final Intent data) {
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
         {
             Bundle extras = data.getExtras();
-            Bitmap picture = (Bitmap)extras.get("data");
-            picturePreview.setImageBitmap(picture);
+            if(extras != null){
+                Bitmap picture = (Bitmap)extras.get("data");
+                picturePreview.setImageBitmap(picture);
 
-            //Compress bitmap to Base64 so that Vision API can process it
-            ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-            picture.compress(Bitmap.CompressFormat.JPEG,90,byteStream);
+                //Compress bitmap to Base64 so that Vision API can process it
+                ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
 
-            //Get Base64 String
-            String base64Data = Base64.encodeToString(byteStream.toByteArray(),Base64.URL_SAFE);
+                picture.compress(Bitmap.CompressFormat.JPEG,90,byteStream);
 
-            //Send Request to Vision API
-            String requestURI = "https://vision.googleapis.com/v1/images:annotate?key="
-                    + getResources().getString(R.string.myKey);
 
-            //Create an array containing
-            //the LABEL_DETECTION feature
-            JSONArray features = new JSONArray();
-            JSONObject feature = new JSONObject();
-            try {
-                feature.put("type","LABEL_DETECTION");
-                features.put(feature);
-            } catch (JSONException e) {
-                e.printStackTrace();
+                //Get Base64 String
+                String base64Data = Base64.encodeToString(byteStream.toByteArray(),
+                        Base64.URL_SAFE);
+
+                //Send Request to Vision API
+                String requestURL =
+                        "https://vision.googleapis.com/v1/images:annotate?key=" +
+                                getResources().getString(R.string.myKey);
+                try {
+                //Create an array containing
+                //the LABEL_DETECTION feature
+                    JSONArray features = new JSONArray();
+                    JSONObject feature = new JSONObject();
+                    feature.put("type", "LABEL_DETECTION");
+                    features.put(feature);
+
+                    // Create an object containing
+                    // the Base64-encoded image data
+                    JSONObject imageContent = new JSONObject();
+                    imageContent.put("content", base64Data);
+
+                    // Put the array and object into a single request
+                    // and then put the request into an array of requests
+                    JSONArray requests = new JSONArray();
+                    JSONObject request = new JSONObject();
+                    request.put("image", imageContent);
+                    request.put("features", features);
+                    requests.put(request);
+                    JSONObject postData = new JSONObject();
+                    postData.put("requests", requests);
+
+                    // Convert the JSON into a
+                    // string
+                    String body = postData.toString();
+
+                    Fuel.post(requestURL)
+                            .header(
+                                    new Pair<String, Object>("content-length", body.length()),
+                                    new Pair<String, Object>("content-type", "application/json")
+                            )
+                            .body(body.getBytes())
+                            .responseString(new Handler<String>() {
+                                @Override
+                                public void success(@NotNull Request request,
+                                                    @NotNull Response response,
+                                                    String data) {
+                                    // Access the labelAnnotations arrays
+                                    try {
+                                       JSONArray labels = new JSONObject(data)
+                                                .getJSONArray("responses")
+                                                .getJSONObject(0)
+                                                .getJSONArray("labelAnnotations");
+                                        String results = "";
+
+                                        // Loop through the array and extract the
+                                        // description key for each item
+                                        for(int i=0;i<labels.length();i++) {
+                                            results = results +
+                                                    labels.getJSONObject(i).getString("description") +
+                                                    "\n";
+                                        }
+
+                                    // Display the annotations inside the TextView
+                                       textResult.setText(results);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+
+                                @Override
+                                public void failure(@NotNull Request request,
+                                                    @NotNull Response response,
+                                                    @NotNull FuelError fuelError) {}
+                            });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
             }
-
-            //Create an object containing
-            //the Base64-encoded image data
-            JSONObject imageContent = new JSONObject();
-            try {
-                imageContent.put("content",base64Data);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            //Put the array and object into a single request
-            //and then put the request into an array of requests
-            JSONObject request = new JSONObject();
-            try {
-                request.put("image",imageContent);
-                request.put("features",features);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            JSONArray requests = new JSONArray();
-            requests.put(request);
-
-            JSONObject postData = new JSONObject();
-            try {
-                postData.put("requests",postData);
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-            //Convert JSON into a string
-            String body = postData.toString();
-
-            //Using Fuel to post request and get responses
-
-
         }
     }
 }
