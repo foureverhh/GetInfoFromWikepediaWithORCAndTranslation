3175603707f3edba20898abd20a7789d8ffa4ae0
foureverhh <foureverhh@gmail.com>
2018-11-05 21:56:20 +0100

    Wrate an Vision API request JSON

--- app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/TranslateTextFromCamera.java
+++ app/src/main/java/com/example/zfgg04/getinfofromwikepediawithorcandtranslation/TranslateTextFromCamera.java
@@ -11,6 +11,10 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.ByteArrayOutputStream;
 
 public class TranslateTextFromCamera extends AppCompatActivity {
@@ -49,8 +53,58 @@ public class TranslateTextFromCamera extends AppCompatActivity {
             ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
             picture.compress(Bitmap.CompressFormat.JPEG,90,byteStream);
 
-             //Get Base64 String
+            //Get Base64 String
             String base64Data = Base64.encodeToString(byteStream.toByteArray(),Base64.URL_SAFE);
+
+            //Send Request to Vision API
+            String requestURI = "https://vision.googleapis.com/v1/images:annotate?key="
+                    + getResources().getString(R.string.myKey);
+
+            //Create an array containing
+            //the LABEL_DETECTION feature
+            JSONArray features = new JSONArray();
+            JSONObject feature = new JSONObject();
+            try {
+                feature.put("type","LABEL_DETECTION");
+                features.put(feature);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //Create an object containing
+            //the Base64-encoded image data
+            JSONObject imageContent = new JSONObject();
+            try {
+                imageContent.put("content",base64Data);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //Put the array and object into a single request
+            //and then put the request into an array of requests
+            JSONObject request = new JSONObject();
+            try {
+                request.put("image",imageContent);
+                request.put("features",features);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            JSONArray requests = new JSONArray();
+            requests.put(request);
+
+            JSONObject postData = new JSONObject();
+            try {
+                postData.put("requests",postData);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            //Convert JSON into a string
+            String body = postData.toString();
+
+            //Using Fuel to post request and get responses
+
 
         }
     }
